afb622b0a5f16ef849e41eb7e80f28895d9b6a17
Daniel González Cerviño <daniel@desarrolla2.com>
2016-07-22 13:01:47 +0200

    [PROT-2059] -

--- phpstorm/WebIde70/config/fileTemplates/includes/PHP File Header.php
+++
@@ -1,8 +0,0 @@
-/**
- * This file is part of the ${PROJECT_NAME}.
- *
- * (c) Daniel González <daniel@desarrolla2.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -2,6 +2,11 @@
 export ZSH=$HOME/.oh-my-zsh
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:~/.local/bin"
 
-ZSH_THEME="bira"
-plugins=(git cap composer compleat symfony2 tmux)
+ZSH_THEME="desarrolla2"
+#ZSH_THEME="amuse" # (this is one of the fancy ones)
+plugins=(git cap composer compleat symfony2 tmux vagrant)
+
 source $ZSH/oh-my-zsh.sh
+source ~/.local/oh-my-zsh/git-flow.plugin.zsh
+source ~/.bash_profile
+
