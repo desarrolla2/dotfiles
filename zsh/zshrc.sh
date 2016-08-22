52ac6c968dda32adc6d3662238cbc360bf13076a
Daniel González <daniel@desarrolla2.com>
2016-08-22 13:13:55 +0200

    added docker and react native plugins to ohmyzsh

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -3,7 +3,7 @@ export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/g
 
 ZSH_THEME="desarrolla2"
 #ZSH_THEME="amuse" # (this is one of the fancy ones)
-plugins=(git cap composer compleat symfony2 tmux vagrant)
+plugins=(git cap composer compleat symfony2 tmux vagrant react-native docker)
 
 source $ZSH/oh-my-zsh.sh
 #source ~/.bash_profile
