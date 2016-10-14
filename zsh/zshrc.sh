429e889192744814165195fc3ba0cb1bbfe22334
Daniel González Cerviño <daniel@desarrolla2.com>
2016-10-14 17:52:48 +0200

    [] - updated /home/dgonzalez/Android/Sdk/tools:/home/dgonzalez/Android/Sdk/platform-tools:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:~/.local/bin:/home/dgonzalez/.composer/vendor/bin

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,5 +1,5 @@
 export ZSH=$HOME/.oh-my-zsh
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:~/.local/bin"
+export PATH=$PATH:~/.local/bin:./node_modules/.bin
 
 ZSH_THEME="desarrolla2"
 #ZSH_THEME="amuse" # (this is one of the fancy ones)
