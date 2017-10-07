3f21aa9bf50e47dacbc48a0433b86063985a42ec
Daniel González <daniel@devtia.com>
2017-10-07 10:28:43 +0200

    [] - added ng plugin

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -4,7 +4,7 @@ export PATH=$PATH:~/.local/bin:./node_modules/.bin:./vendor/bin:$GOPATH/bin
 
 ZSH_THEME=desarrolla2
 #ZSH_THEME="amuse" # (this is one of the fancy ones)
-plugins=(git composer compleat symfony2 tmux vagrant react-native docker grunt)
+plugins=(git composer compleat symfony2 tmux vagrant react-native docker grunti ng)
 
 source $ZSH/oh-my-zsh.sh
 
@@ -12,3 +12,7 @@ if [ -f ~/.bash_profile ]
 then
   source ~/.bash_profile
 fi
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
