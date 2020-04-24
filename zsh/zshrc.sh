8e50df230a5b7df11c1651aaa4afeb84939537ae
Daniel González <daniel@devtia.com>
2020-04-24 15:38:34 +0200

    remove go from path

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,6 +1,5 @@
 export ZSH=$HOME/.oh-my-zsh
-export GOPATH=~/go
-export PATH=$PATH:~/.local/bin:./node_modules/.bin:./vendor/bin:$GOPATH/bin
+export PATH=$PATH:~/.local/bin:./node_modules/.bin:./vendor/bin
 
 ZSH_THEME=desarrolla2
 #ZSH_THEME="amuse" # (this is one of the fancy ones)
